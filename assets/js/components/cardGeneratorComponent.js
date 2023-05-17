56048bc45c98a5554f7537fea9efd9678df3a825
_ <pedropmancillac@gmail.com>
2023-05-17 18:53:41 -0400

    fix mouseenter rapido

--- assets/js/components/cardGeneratorComponent.js
+++ assets/js/components/cardGeneratorComponent.js
@@ -2,31 +2,34 @@ import swapi from "../API/swapi.js"
 import CharacterCard from "../class/CharacterCard.js"
 
 const getPeople = function* (card_generator) {
-    const { color, range, state } = card_generator.dataset
-
-    if (state) return;
+    const { color, range } = card_generator.dataset
 
     const [start, end, _] = range.split(',').map(Number)
     for (let i = start; i <= end; i++) {
         card_generator.dataset.state = "loading"
-        yield swapi(i).then(data => {
-            try {
-                card_generator.parentElement.insertAdjacentHTML(
-                    'beforeend',
-                    new CharacterCard({ ...data, color }).html()
-                )
-            }
-            catch (error) { console.log(error) }
-            card_generator.dataset.state = ""
-        })
+        yield swapi(i)
+            .then(data => {
+                try {
+                    card_generator.parentElement.insertAdjacentHTML(
+                        'beforeend',
+                        new CharacterCard({ ...data, color }).html()
+                    )
+                }
+                catch (error) { console.log(error) }
+                card_generator.dataset.state = ""
+            })
     }
-    card_generator.dataset.state = "loaded"
+    card_generator.dataset.state = "completed"
 }
 
 export default () =>
     document
-        .querySelectorAll('.card[data-card-generator] .card__body')
-        .forEach(card_body => {
-            const generator = getPeople(card_body.parentElement)
-            card_body.addEventListener('mouseenter', () => generator.next())
+        .querySelectorAll('.card[data-card-generator]')
+        .forEach(card => {
+            const generator = getPeople(card)
+            card.querySelector(".card__body")
+                .addEventListener(
+                    'mouseenter',
+                    () => !card.dataset.state && generator.next()
+                )
         })
