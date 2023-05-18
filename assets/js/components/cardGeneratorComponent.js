8d33ce01ec99139743ce11af2a97ed9fff3b745d
_ <pedropmancillac@gmail.com>
2023-05-18 17:17:04 -0400

    mejorando responsive

--- assets/js/components/cardGeneratorComponent.js
+++ assets/js/components/cardGeneratorComponent.js
@@ -1,35 +1,40 @@
 import swapi from "../API/swapi.js"
 import CharacterCard from "../class/CharacterCard.js"
 
-const getPeople = function* (card_generator) {
-    const { color, range } = card_generator.dataset
+function* getPeople(card_generator) {
+    const
+        { color, range } = card_generator.dataset,
+        [start, end, _] = range.split(',').map(Number)
 
-    const [start, end, _] = range.split(',').map(Number)
-    for (let i = start; i <= end; i++) {
-        card_generator.dataset.state = "loading"
-        yield swapi(i)
-            .then(data => {
-                try {
-                    card_generator.parentElement.insertAdjacentHTML(
-                        'beforeend',
-                        new CharacterCard({ ...data, color }).html()
-                    )
-                }
-                catch (error) { console.log(error) }
-                card_generator.dataset.state = ""
-            })
+    const onFulfilled = data => {
+        try {
+            card_generator.parentElement
+                .insertAdjacentHTML(
+                    'beforeend',
+                    new CharacterCard({ ...data, color }).html()
+                )
+        }
+        catch (error) { console.log(error) }
     }
-    card_generator.dataset.state = "completed"
+
+    for (let i = start; i <= end; i++) yield (
+        (card_generator.dataset.state = "loading")
+        &&
+        swapi(i)
+            .then(onFulfilled)
+            .then(() => card_generator.dataset.state = i == end ? "completed" : "")
+    )
 }
 
 export default () =>
     document
         .querySelectorAll('.card[data-card-generator]')
         .forEach(card => {
-            const generator = getPeople(card)
-            card.querySelector(".card__body")
-                .addEventListener(
-                    'mouseenter',
-                    () => !card.dataset.state && generator.next()
-                )
+            const
+                generator = getPeople(card),
+                cardBody = card.querySelector(".card__body"),
+                listener = () => !card.dataset.state && generator.next()
+
+            cardBody.addEventListener('mouseenter', listener)
+            cardBody.addEventListener("click", listener)
         })
